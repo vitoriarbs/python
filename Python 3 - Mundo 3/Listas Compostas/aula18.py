4b4708bc2ae0644fbbbb7c0f17a808b06aafad87
vitoriarbs <160158820+vitoriarbs@users.noreply.github.com>
2024-07-04 21:33:41 -0300

    cursoemvideo

--- Python 3 - Mundo 3/Listas Compostas/aula18.py
+++ Python 3 - Mundo 3/Listas Compostas/aula18.py
@@ -1,7 +1,33 @@
 dados = []
-
 dados.append("Pedro")
 dados.append(25)
 print(dados[0])
 
-p
+pessoas = [["Ana", 33], ["João", 19], ["Joaquim", 13]]
+pessoas.append(dados[:])
+dados.clear()
+pessoas[1] = ["Maria", 19]
+
+#mostrando dados compostos
+#print(pessoas)
+#print(pessoas[1])
+#print(pessoas[2][0])
+for p in pessoas:
+    print(p)
+
+#adicionando dado com repetção
+#for c in range(0,2):
+#    dados.append(str(input("Nome: ")))
+#    dados.append(int(input("Idade: ")))
+#    pessoas.append(dados[:])
+#    dados.clear()
+
+for p in pessoas:
+    #p[1] é onde fica a idade
+    if p[1] >= 21:
+        #p[0] é onde ficam os nomes
+        print(f"{p[0]} é maior de idade")
+    
+    else:
+        #p[0] é onde ficam os nomes
+        print(f"{p[0]} é menor de idade")
